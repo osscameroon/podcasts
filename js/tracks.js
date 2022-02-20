838937defaf2ad73fa91f0ff7d0f3250732ec0a3
sanix-darker <s4nixd@gmail.com>
2022-02-20 10:09:09 +0100

    feat: testsing stuffs

--- js/tracks.js
+++ js/tracks.js
@@ -4,12 +4,12 @@ const podcasts = [
         "track": 1,
         "name": "OSS Talk Fluter Vs ReactNative | 02-04-2022",
         "duration": "2:04:30",
-        "file": "/tracks/flutervsreactnative.ogg"
+        "file": "/tracks/flutervsreactnative.mp3"
     },
     {
         "track": 2,
         "name": "OSS Talk Bad applications / Responsabilities | 02-11-2022",
         "duration": "1:42:07",
-        "file": "/tracks/bad-app-and-responsabilities.ogg"
+        "file": "/tracks/bad-app-and-responsabilities.mp3"
     },
 ]
