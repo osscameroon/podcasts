c3efee778ed476dddd77ffc62c566dc6bdd3a858
yokwejuste <yokwejuste@yahoo.com>
2022-04-12 00:58:22 +0100

    updates some files

--- podcasts-oss/src/components/Header/Items/Items.js
+++ podcasts-oss/src/components/Header/Items/Items.js
@@ -5,14 +5,17 @@ const MenuItems = [
     {
         'item_name': 'Courses',
         'item_url': '#courses',
+        'separation': '|',
     },
     {
         'item_name': 'Podcasts',
-        'item_url': '#podcasts'
+        'item_url': '#podcasts',
+        'separation': '|',
     },
     {
         'item_name': 'Blog',
-        'item_url': '#blog'
+        'item_url': '#blog',
+        'separation': '',
     }
 ]
 
@@ -20,16 +23,20 @@ class Items extends React.Component {
     render() {
         return (
             <div className="items">
-                {
-                    MenuItems.map((item, index) => {
-                            return (
-                                <ul className="item" key={index}>
-                                    <li><a href={item.item_url}>{item.item_name}</a></li>
-                                </ul>
-                            )
-                        }
-                    )
-                }
+                <ul className="item">
+                    {
+                        MenuItems.map((item, index) => {
+                                return (
+                                    <li>
+                                        <a href={item.item_url} key={index}>{item.item_name}</a>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item.separation}&nbsp;&nbsp;&nbsp;&nbsp;
+                                    </li>
+                                )
+                            }
+                        )
+                    }
+                </ul>
+                <p className="btn btn-outline-light rounded-circle mx-5 my-4 p-mx-2">EN</p>
             </div>
         )
     }
