41da79ffb161332f981da3f5d10ed36039ad22e2
godsakani <nyonggodwill11@gmail.com>
2022-04-15 23:35:23 +0100

    added tracks for podcast list

---
+++ podcasts-oss/src/tracks/tracks.js
@@ -0,0 +1,22 @@
+export const track_oss = [
+    {
+        title: '01. OSS Talk Flutter Vs ReactNative',
+        podName: 'The OSS Community - 02/04/2022',
+        fileUrl: '',
+    },
+    {
+        title: '01. OSS Talk Flutter Vs ReactNative',
+        podName: 'The OSS Community - 02/04/2022',
+        fileUrl: '',
+    },
+    {
+        title: '01. OSS Talk Flutter Vs ReactNative',
+        podName: 'The OSS Community - 02/04/2022',
+        fileUrl: '',
+    },
+    {
+        title: '01. OSS Talk Flutter Vs ReactNative',
+        podName: 'The OSS Community - 02/04/2022',
+        fileUrl: '',
+    },
+]
