9cd0d52943430b93377393be38e7cd91f82d6abc
godsakani <nyonggodwill11@gmail.com>
2022-04-16 22:25:59 +0100

    added imports of other components

--- podcasts-oss/src/App.js
+++ podcasts-oss/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from './components/Header/Header';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './fonts/GoogleSans-Bold-v1.27.ttf';
+import './fonts/GoogleSans-Medium-v1.27.ttf';
+import './fonts/GoogleSans-Regular-v1.27.ttf';
 import Button from './components/Button/Button';
 import PodcastList from './components/podcasts_list/PodcastList';
 
@@ -9,8 +12,8 @@ function App() {
     return (
         <div className="App">
             <Header/>
-            <Button/>
             <PodcastList/>
+            <Button/>
         </div>
 
     );
