4ffe3e1a72313c3a8bc94632e72a22a04e58c661
godsakani <nyonggodwill11@gmail.com>
2022-04-25 16:04:35 +0100

    import styles for podcastList

--- podcasts-oss/src/components/Header/Logo/Logo.js
+++ podcasts-oss/src/components/Header/Logo/Logo.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import podcast_logo from '../../../assets/podcast_logo.png';
 import './Logo.css';
+import '../../podcasts_list/PodcastList.css'
 
 class Logo extends Component {
     render() {
@@ -9,6 +10,7 @@ class Logo extends Component {
                 <a href="/">
                     <img src={podcast_logo} alt="Podcasts" className="header_logo_image"/>
                 </a>
+                
             </div>
         );
     }
