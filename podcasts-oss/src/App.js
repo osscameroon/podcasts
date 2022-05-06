b2c6c16bfe5e841b48d57aa5428a0142b1b594ff
godsakani <nyonggodwill11@gmail.com>
2022-05-06 21:37:40 +0100

    modified app.js file

--- podcasts-oss/src/App.js
+++ podcasts-oss/src/App.js
@@ -8,6 +8,7 @@ import './fonts/GoogleSans-Regular-v1.27.ttf';
 import Button from './components/Button/Button';
 import PodcastList from './components/podcasts_list/PodcastList';
 
+
 function App() {
     return (
         <div className="App">
