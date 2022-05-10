baaaefa8087969aa1a27f5a5a269edf7a647c5aa
yokwejuste <yokwejuste@yahoo.com>
2022-05-10 21:49:39 +0100

    Updated the header for scroll view effect.

--- podcasts-oss/src/components/Header/Header.js
+++ podcasts-oss/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect }from 'react';
 import './Header.css';
 import '../Header/searchForm/searchForm.css';
 import podcast_logo from '../../assets/podcast_logo.png';
@@ -27,9 +27,27 @@ export const Header = () => {
         setTitle(keyword);
     };
 
+
+    const [navSize, setnavSize] = useState("10rem");
+    const [navColor, setnavColor] = useState("transparent");
+    const listenScrollEvent = () => {
+        window.scrollY > 10 ? setnavColor("#0023ff") : setnavColor("transparent");
+        window.scrollY > 10 ? setnavSize("10rem") : setnavSize("10rem");
+    };
+    useEffect(() => {
+        window.addEventListener("scroll", listenScrollEvent);
+        return () => {
+            window.removeEventListener("scroll", listenScrollEvent);
+        };
+    }, []);
+
     return (
         <>
-            <div className="header">
+            <div className="header" style={{
+                backgroundColor: navColor,
+                height: navSize,
+                transition: "all 1s"
+            }}>
                 <nav className="navbar navbar-expand-lg navbar-light">
                     <div className="container-fluid">
                         <div className="header_logo">
@@ -56,7 +74,7 @@ export const Header = () => {
                                         />
                                     </form>
                                 </div>
-                                <a href="" className="nav-item nav-link active">
+                                <a href="/" className="nav-item nav-link active">
                                     <i className="fa fa-sort-amount-asc sort"/>
                                 </a>
                             </div>
