95a9722b509e691e9f02f849e45db06d48919884
godsakani <nyonggodwill11@gmail.com>
2022-05-17 19:57:03 +0100

    feat: added sort button and sort function

--- podcasts-oss/src/components/Header/Header.js
+++ podcasts-oss/src/components/Header/Header.js
@@ -4,12 +4,20 @@ import '../Header/searchForm/searchForm.css';
 import podcast_logo from '../../assets/podcast_logo.png';
 import { track_oss } from '../../tracks/tracks';
 import { PodcastList } from '../podcasts_list/PodcastList';
-import { Button } from '../Button/Button';
-
-
 
 export const Header = () => {
 
+    const sortHandler = () => {
+        console.log("Clicked");
+        const sortedt = track_oss.map((t) => {
+            t.dateObj = new Date(t.date)
+            return t;
+        }).sort((a, b) => {
+            return b.dateObj - a.dateObj;
+        });
+        console.log(sortedt)
+        //console.log(track_oss.sort((a,b)=> new Date(a.date) > new Date(b.date)))
+    }
     
 
     const [title, setTitle] = useState('');
@@ -45,50 +53,47 @@ export const Header = () => {
         };
     }, []);
 
-    return (
-        <>
-            <div className="header" style={{
-                backgroundColor: navColor,
-                height: navSize,
-                transition: "all 1s"
-            }}>
-                <nav className="navbar navbar-expand-lg navbar-light">
-                    <div className="container-fluid">
-                        <div className="header_logo">
-                            <a href="/">
-                                <img src={podcast_logo} alt="Podcasts" className="header_logo_image"/>
-                            </a>
-                        </div>
-                        <button type="button" className="navbar-toggler" data-bs-toggle="collapse"
-                                data-bs-target="#navbarCollapse">
-                            <span>
-                                <i className="fa fa-search"/>
-                            </span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarCollapse">
-                            <div className="navbar-nav">
-                                <div>
-                                     <form className="no_submit mx-5">
-                                        <input
-                                            className="no_submit col-xs-4 px-5 input-sm"
-                                            type="search"
-                                            value={title}
-                                            placeholder="Search podcasts..."
-                                            onChange={filter}
-                                        />
-                                    </form>
-                                </div>
-                                <Button/>
-                                {/* <a href="/" className="nav-item nav-link active">
+    return <>
+        <div className="header" style={{
+            backgroundColor: navColor,
+            height: navSize,
+            transition: "all 1s"
+        }}>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container-fluid">
+                    <div className="header_logo">
+                        <a href="/">
+                            <img src={podcast_logo} alt="Podcasts" className="header_logo_image"/>
+                        </a>
+                    </div>
+                    <button type="button" className="navbar-toggler" data-bs-toggle="collapse"
+                            data-bs-target="#navbarCollapse">
+                        <span>
+                            <i className="fa fa-search"/>
+                        </span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <div className="navbar-nav">
+                            <div>
+                                 <form className="no_submit mx-5">
+                                    <input
+                                        className="no_submit col-xs-4 px-5 input-sm"
+                                        type="search"
+                                        value={title}
+                                        placeholder="Search podcasts..."
+                                        onChange={filter}
+                                    />
+                                    <span className="fa fa-sort-amount-asc sort btn"
+                                        onClick={sortHandler()}>
+                                    </span>
+                                </form>
                                 
-                                    <i className="fa fa-sort-amount-asc sort"/>
-                                </a>  */}
                             </div>
                         </div>
                     </div>
-                </nav>
-            </div>
-            <PodcastList list={podCastItems}/>
-        </>
-    );
-};
+                </div>
+            </nav>
+        </div>
+        <PodcastList list={podCastItems}/>
+    </>;
+}
