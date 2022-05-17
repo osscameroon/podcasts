5c9abd0c67b467bdbfc4bd9b68ba09b5201f9760
godsakani <nyonggodwill11@gmail.com>
2022-05-17 20:01:10 +0100

    fix: remove button function from app component

--- podcasts-oss/src/App.js
+++ podcasts-oss/src/App.js
@@ -5,14 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './fonts/GoogleSans-Bold-v1.27.ttf';
 import './fonts/GoogleSans-Medium-v1.27.ttf';
 import './fonts/GoogleSans-Regular-v1.27.ttf';
-import { Button } from './components/Button/Button';
 
 
 function App() {
     return (
         <div className="App">
             <Header/>
-            <Button/>
         </div>
 
     );
--- podcasts-oss/src/components/Button/Button.js
+++ podcasts-oss/src/components/Button/Button.js
@@ -1,41 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { track_oss } from '../../tracks/tracks'
+// import React from "react";
 
 
 export const Button = () => {
-    
-    const [ sortPodcast, setSortPodcast] = useState([]);
 
-    const [ sortType, setsortType ] = useState();
-
-    useEffect(() => {
-         
-        const sortItem = type => {
-
-            const types = { track_oss};
-
-            const sortProperty = types[type];
-            const sorted = [ track_oss].sort((a, b)=>
-            b[sortProperty] - a[sortProperty]);
-            setSortPodcast(sorted);
-        };
-        sortItem(sortType);
-     
-      return () => {
-        <div>
-        <a href="/" className="nav-item nav-link active">
-                                
-        <i className="fa fa-sort-amount-asc sort" onClick= {(e) => setsortType(e.target.value)}/>
-                                </a> 
-        
-        {sortPodcast.map(track_oss => (
-            <div>
-            <span className="track-title"> {track_oss.id}- {track_oss.title}</span><br/>
-            <span className="track-podcast-name text-black-50">{track_oss.podName}</span>
-            </div>
-             ))}
-        </div>
-      };
-    }, )
-
-}
+};
