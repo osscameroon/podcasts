4b0d81a379797611a9c23bc823b4cfb631dbbaef
Steve Yonkeu <yokwejuste@yahoo.com>
2022-05-28 08:51:26 +0100

    Feat: Migrated to fontawesome components

--- src/components/podcasts_list/PodcastList.js
+++ src/components/podcasts_list/PodcastList.js
@@ -2,27 +2,29 @@ import React, {useContext} from "react";
 import './PodcastList.css'
 import Player from "../Player/Player";
 import AppContext from "../AppContext/AppContext";
-import { faMicrophoneLines } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faMicrophoneLines} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
-export const PodCastComponent = ({track, onClick}) => (
-    <li className={"trackContainer " + track.active} onClick={onClick}>
+export const PodCastComponent = ({track, onClick, trunk, trackIndex}) => (
+    <li className={trackIndex === track.id - 1 ?
+        'active trackContainer' : "trackContainer"}
+        onClick={onClick}>
         <div>
             <div className="float-start">
-                <FontAwesomeIcon icon={faMicrophoneLines} style={{fontSize: "3.3em"}} />
+                <FontAwesomeIcon icon={faMicrophoneLines} style={{fontSize: "3.3em"}}/>
             </div>
             <div className="">
                 <span className="track-title">
-                    {track.id}- {track.title}
+                    {track.id}- {trunk}
                 </span><br/>
-                <span className="track-podcast-name text-black-50">
-                {track.podName} {track.date}
-            </span>
+                <p className="track-podcast-name text-black-50 d-none d-sm-none d-md-none d-lg-block">
+                    {track.podName} {track.date}
+                </p>
                 <div className="float-end" id="download">
                     <a href={track.fileUrl} className="border-light bg-transparent text-dark"
                        type="download">
-                        <i className="fa-solid fa-down-to-bracket"  aria-hidden="true"/>
+                        <FontAwesomeIcon icon="fa-solid fa-down-to-bracket" style={{fontSize: "3.3em"}}/>
                     </a>
                 </div>
             </div>
@@ -31,7 +33,8 @@ export const PodCastComponent = ({track, onClick}) => (
 )
 
 export default function PodcastList({list}) {
-    const {setTrackIndex} = useContext(AppContext);
+    const {isActive} = useContext(AppContext);
+    const {trackIndex, setTrackIndex} = useContext(AppContext);
     return (
         <>
             <ul>
@@ -39,8 +42,11 @@ export default function PodcastList({list}) {
                     <div className="row">
                         <div className="col-xs-12 col-sm-8 col-md-8 mx-auto">
                             {list.map((track, i) => <PodCastComponent
-                                    key={i}
+                                    isActive={isActive}
+                                    trunk={track.title}
                                     track={track}
+                                    key={track.id}
+                                    trackIndex={trackIndex}
                                     onClick={function () {
                                         setTrackIndex(Number(track.id) - 1);
                                     }}
@@ -54,5 +60,3 @@ export default function PodcastList({list}) {
         </>
     )
 }
-
-
