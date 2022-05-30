40864b66c08f07f791c729d029e10fbd920f6b9d
godsakani <nyonggodwill11@gmail.com>
2022-05-30 03:13:17 +0100

    feat: added side-contents. added download icons for tracks

--- src/components/podcasts_list/PodcastList.js
+++ src/components/podcasts_list/PodcastList.js
@@ -4,7 +4,8 @@ import Player from "../Player/Player";
 import AppContext from "../AppContext/AppContext";
 import { faMicrophoneLines } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faTelegramPlane } from "@fortawesome/free-brands-svg-icons";
 
 export const PodCastComponent = ({track, onClick}) => (
     <li className={"trackContainer " + track.active} onClick={onClick}>
@@ -22,7 +23,7 @@ export const PodCastComponent = ({track, onClick}) => (
                 <div className="float-end" id="download">
                     <a href={track.fileUrl} className="border-light bg-transparent text-dark"
                        type="download">
-                        <i className="fa-solid fa-down-to-bracket"  aria-hidden="true"/>
+                        <FontAwesomeIcon icon={faDownload} style={{color: "dark", fontSize: "1em", paddingBottom: "20px"}} />
                     </a>
                 </div>
             </div>
@@ -37,6 +38,17 @@ export default function PodcastList({list}) {
             <ul>
                 <div className="container">
                     <div className="row">
+                    <div className="side-bar col-3 col-md-3">
+                            <span className="">
+                                <p id="content">In just a click on the plane below, Join the <span id="Logname">OSS Community</span> <br/> now!</p>
+                                <hr id="line"/>
+                                <span>
+                               <a href="https://t.me/+O7SQfusW8XVmOTFi" className="fa-5x">
+                               <FontAwesomeIcon icon={faTelegramPlane} style={{fontSize: "1em"}} id="telegram"/>
+                              </a> 
+                            </span>
+                            </span>
+                        </div>
                         <div className="col-xs-12 col-sm-8 col-md-8 mx-auto">
                             {list.map((track, i) => <PodCastComponent
                                     key={i}
